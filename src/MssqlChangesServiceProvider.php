fcae7e0d1c0ddd511316e2cf8d479a7e41e2afd8
Sami Walbury <git@mha.systems>
2022-09-20 16:59:23 +0100

    Corrected package name

--- src/MssqlChangesServiceProvider.php
+++ src/MssqlChangesServiceProvider.php
@@ -4,22 +4,13 @@ namespace Patabugen\MssqlChanges;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Patabugen\MssqlChanges\Commands\MssqlChangesCommand;
 
 class MssqlChangesServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
-            ->name('mssqlchanges')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_mssqlchanges_table')
-            ->hasCommand(MssqlChangesCommand::class);
+            ->name('mssql-changes')
+            ->hasConfigFile();
     }
 }
