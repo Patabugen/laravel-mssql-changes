fdeec643d4a1a2c4c5c0f70dfd06950f567a2016
Sami Walbury <sami@patabugen.co.uk>
2022-09-27 16:17:20 +0100

    Add Table::create method to fetch a table

--- src/Table.php
+++ src/Table.php
@@ -4,12 +4,13 @@ namespace Patabugen\MssqlChanges;
 
 use Illuminate\Database\ConnectionInterface;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use Patabugen\MssqlChanges\Actions\ListTables;
 
 class Table
 {
     private Collection $changes;
-
 
     public string $name;
 
@@ -37,4 +38,17 @@ class Table
         ];
     }
 
+    static function create(string $name)
+    {
+        $foundTables = ListTables::make()->setTableFilter([$name])->handle();
+        throw_if (
+            $foundTables->isEmpty(),
+            "A table named '{$name}' with tracking enabled was not found"
+        );
+        throw_if (
+            $foundTables->count() > 1,
+            "More than one table was found matching '{$name}'"
+        );
+        return $foundTables->first();
+    }
 }
---
+++ tests/TableTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Patabugen\MssqlChanges\Tests;
+
+use Patabugen\MssqlChanges\Table;
+
+class TableTest extends TestCase
+{
+    public function test_error_thrown_if_creating_invalid_table()
+    {
+        $this->expectExceptionMessage("A table named 'not-a-real-table' with tracking enabled was not found");
+        Table::create('not-a-real-table');
+    }
+
+    public function test_we_can_create_a_table_from_its_name()
+    {
+        $table = Table::create('Contacts');
+        $this->assertInstanceOf(Table::class, $table);
+        $this->assertEquals('Contacts', $table->name);
+        $this->assertTrue($table->columnTrackingEnabled);
+    }
+}
